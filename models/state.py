b928941dae7f8696467640139f146e5ae4d66242
254jack <taixonj@gmail.com>
2024-01-03 18:31:17 +0300

    models/state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+'''
+    State module
+'''
+
+from models.base_model import BaseModel, Base
+import sqlalchemy
+import models
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    '''
+        State Class
+    '''
+    if getenv('new_TYPE_STORAGE') != 'db':
+
+        @property
+        def cities(self):
+            """Setter Method to list cities"""
+            city_list = []
+            for key, value in models.storage.all().items():
+                try:
+                    if value.state_id == self.id:
+                        city_list.append(value)
+                except BaseException:
+                    pass
+            return city_list
+    else:
+        cities = relationship('City', cascade='all, delete', backref='state')
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
